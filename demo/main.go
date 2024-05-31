0e7351f410e699228235bf1c181d08900c4829da
Hank Shen <swh@admpub.com>
2024-05-31 20:31:45 +0800

    update

--- demo/main.go
+++ demo/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/admpub/caddy/caddy/caddymain"
-	_ "github.com/caddy-plugins/caddy-recaptcha"
+	_ "github.com/caddy-plugins/caddy-recaptcha/recaptcha"
 )
 
 func main() {
